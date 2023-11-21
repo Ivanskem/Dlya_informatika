b7575c36612f909db93c2c88db1a62e865ff1cc8
Ivanskem <150799423+Ivanskem@users.noreply.github.com>
2023-11-21 17:47:37 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import time
+wallet = 0
+tomato = 50
+ogurec = 45
+cheeze = 50
+print("Build: her znaet kakoy.")
+print("User: Naydi sam.")
+time.sleep(3)
+print("If you want to continue write continue")
+if str(input()) == "continue":
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+else:
+    exit()
+peperoni = 500
+while True:
+    print("Здравствуйте. Вы попали на тест пицерии. Напишите пиццу которую хотите заказать")
+    print("Напишите название пиццы которую хотите заказать: ")
+    if str(input()) == "peperoni":
+        print("Товар добавлен в корзину")
+        wallet += 500
+    print("Если вы хотите добавить что-нибудь в пиццу, напишите продолжить")
+    print("Если вы не хотите, то напишите Перейти к оплате")
+    if str(input()) == "продолжить":
+        if str(input("напишите что хотите добавить: ")) == "сыр":
+
+
+    break
